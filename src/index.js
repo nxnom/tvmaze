918f7dd5aa733d7acb5bcb2c460316c92569470c
EBrain <ebrain.ebrain@gmail.com>
2023-02-06 21:55:11 +0630

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import './index.css';
 import './popup.css';
+import { fetchItemDetails, openPopup } from './modules/popup.js';
+
+const btn = document.createElement('button');
+
+btn.innerHTML = 'Click me and check the console!';
+
+const popup = document.createElement('div');
+
+btn.addEventListener('click', async () => {
+  const details = await fetchItemDetails(1);
+  openPopup(details, popup);
+});
+
+document.body.appendChild(btn);
 
 const add = (a, b) => a + b;
 
