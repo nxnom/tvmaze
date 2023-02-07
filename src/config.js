f87979bd790c3b036f458df61f31379635810550
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-02-07 11:54:33 +0200

    Create app id

--- src/config.js
+++ src/config.js
@@ -2,3 +2,4 @@ export const API_URL = 'https://api.tvmaze.com/shows';
 
 // prettier-ignore
 export const INVOLVEMENT_API_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+export const appID = 'z5zYFp0GHveTpiCf00gq';
