888c43a4a937592b3435b9c07b18c3386df1119b
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-02-07 17:28:58 +0200

    Resolve linters

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import './index.css';
 import './popup.css';
-import { renderShowsToDOM, renderLikesToDOM } from './modules/renderToDOM.js';
-
-const showsContainer = document.querySelector('.maze__grid');
-
-showsContainer.addEventListener('click', (e) => {
-  if (e.target.closest('.comment__btn')) {
-    const { id } = e.target.closest('.comment__btn').closest('.maze__card');
-  }
-});
+import { renderShowsToDOM } from './modules/renderToDOM.js';
 
 renderShowsToDOM();
-renderLikesToDOM();
