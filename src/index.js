2dc2b7ba7397d260ce180e37b69f3a02e8395a27
EBrain <ebrain.ebrain@gmail.com>
2023-02-07 19:47:46 +0630

    Add comments popup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import './index.css';
 import './popup.css';
+import './loader.css';
 import { renderShowsToDOM } from './modules/renderToDOM.js';
+import { createPopup } from './modules/popup.js';
 
 const showsContainer = document.querySelector('.maze__grid');
 
 showsContainer.addEventListener('click', (e) => {
-  if (e.target.closest('.comment__btn')) {
-    const id = e.target.closest('.comment__btn').closest('.maze__card').id;
+  const commentBtn = e.target.closest('.comment__btn');
+
+  if (commentBtn) {
+    const { id } = commentBtn.closest('.maze__card');
+    createPopup(id);
   }
 });
 
